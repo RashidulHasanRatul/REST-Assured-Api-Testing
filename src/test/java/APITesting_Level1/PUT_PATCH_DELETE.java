5b4e49a12da401bab27be8b74d81c913eaba334b
Rashidul Hasan <49413046+RashidulHasanRatul@users.noreply.github.com>
2021-09-19 21:58:15 +0600

    put patch delete

--- src/test/java/APITesting_Level1/PUT_PATCH_DELETE.java
+++ src/test/java/APITesting_Level1/PUT_PATCH_DELETE.java
@@ -1,5 +1,73 @@
 package APITesting_Level1;
+import io.restassured.http.ContentType;
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 public class PUT_PATCH_DELETE {
+
+    @Test
+    public static void PUT ()  {
+
+        baseURI="https://reqres.in/api";
+
+        JSONObject request = new JSONObject();
+        request.put("Name","Rashidul Hasan");
+        request.put("Job","FullStack QA");
+
+
+        given()
+                .header("Content-Type", "application/json")
+                .contentType(ContentType.JSON)
+                .body(request.toJSONString())
+                .when()
+                .put("/users/2")
+                .then()
+                .statusCode(200)
+                .log().all();
+
+    }
+
+
+    @Test
+    public static void PATCH ()  {
+
+        baseURI="https://reqres.in/";
+
+        JSONObject request = new JSONObject();
+        request.put("Name","Rashidul Hasan");
+        request.put("Job","FullStack QA");
+
+
+        given()
+                .header("Content-Type", "application/json")
+                .contentType(ContentType.JSON)
+                .body(request.toJSONString())
+                .when()
+                .put("api/users/2")
+                .then()
+                .statusCode(200)
+                .log().all();
+
+    }
+
+
+    @Test
+    public static void DELETE ()  {
+
+        baseURI="https://reqres.in";
+
+                when()
+                .put("/api/users/2")
+                .then()
+                .statusCode(200)
+                .log().all();
+
+    }
+
 
 }
